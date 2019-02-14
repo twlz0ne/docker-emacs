d595aadb2f46a419d0b46040e14c1ddf3d256ff4
Brady Trainor <mail@bradyt.com>
2019-02-13 22:41:05 -0800

    Try to reproduce flycheck issue

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,29 +1,9 @@
 
-(require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-(let ((pkgs-to-install
-       (seq-remove 'package-installed-p '(dart-mode eglot))))
-  (when pkgs-to-install
-    (package-refresh-contents)
-    (dolist (pkg pkgs-to-install)
-      (package-install pkg))))
-
-(defun project-try-dart (dir)
-  (let ((project (or (locate-dominating-file dir "pubspec.yaml")
-                     (locate-dominating-file dir "BUILD"))))
-    (if project
-        (cons 'dart project)
-      (cons 'transient dir))))
-(add-hook 'project-find-functions #'project-try-dart)
-(cl-defmethod project-roots ((project (head dart)))
-  (list (cdr project)))
-
-(with-eval-after-load "eglot"
-  (add-to-list 'eglot-server-programs
-               '(dart-mode . ("dart_language_server"))))
-
-(add-hook 'dart-mode-hook 'eglot-ensure)
+(unless (package-installed-p 'flycheck)
+  (package-refresh-contents)
+  (package-install 'flycheck))
--- Dockerfile
+++ Dockerfile
@@ -9,17 +9,5 @@ RUN add-apt-repository ppa:kelleyk/emacs \
     && apt-get update && apt-get install -y \
        emacs26-nox \
        && rm -rf /var/lib/apt/lists/*
-RUN curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list \
-    && curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list > /etc/apt/sources.list.d/dart_unstable.list
-RUN apt-get update && apt-get install -y \
-        dart \
-        && rm -rf /var/lib/apt/lists/*
-ENV PATH="/usr/lib/dart/bin:${PATH}"
-ENV PATH="/root/.pub-cache/bin:${PATH}"
-RUN pub global activate yaml \
-  && pub global activate dart_language_server
 ADD .emacs.d /root/.emacs.d
 RUN emacs -batch -l ~/.emacs.d/init.el
-WORKDIR /root/.pub-cache/hosted/pub.dartlang.org/yaml-2.1.15/lib/src
-CMD emacs event.dart
