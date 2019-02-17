14e7727894e9b44029b50746735f63c536697ca0
Brady Trainor <mail@bradyt.com>
2019-02-17 00:13:13 -0800

    Prove use-package can install org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,13 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+        ("melpa" . "https://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
-(unless (package-installed-p 'flycheck)
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'flycheck))
+  (package-install 'use-package))
+
+(use-package org
+  :ensure org-plus-contrib)
